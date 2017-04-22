9c301596c23fbda7192f17a0e49656cfc262b76c
sajen87 <bolsik@gmail.com>
2017-04-22 16:02:33 +0200

    ver.01 changes in names in main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $(function () {
         { firstName: "Jan", lastName: "Kowalski" },
         { firstName: "Adam", lastName: "Nowak" },
         { firstName: "Andrzej", lastName: "Kielcz" }
+        { firstName: "Pawel", lastName: "Kielcz" }
+        { firstName: "Janek", lastName: "Kielcz" }
+        { firstName: "Waldek", lastName: "Kielcz" }
     ];
 
     patientsList.forEach(function (patient) {
